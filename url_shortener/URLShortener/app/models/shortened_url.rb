dad01dfcc4d479ace861b73a322c6e20010f7024
Brekke Green <brekke.andrew.green@gmail.com>
2021-01-28 17:05:15 -0500

    Created models

---
+++ url_shortener/URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :bigint           not null, primary key
+#  long_url   :string           not null
+#  short_url  :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ShortenedUrl < ApplicationRecord
+    belongs_to( :user,
+        primary_key: id,
+        foreign_key: :user_id,
+        class_name: :User 
+    )
+
+end 
+
+# s1 = ShortenedUrl.new(long_url: "thisisalongurl", short_url: "short") 
--- url_shortener/URLShortener/app/models/user.rb
+++ url_shortener/URLShortener/app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    has_many( :shortened_urls
+        primary_key: id,
+        foreign_key: :user_id,
+        class_name: :ShortenedUrl
+    )
+
     validates :email, presence: true, uniqueness: true
 end 
